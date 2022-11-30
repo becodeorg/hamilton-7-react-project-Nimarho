67265e10ced0307c088fe7eb4715110492e6a4ab
Nimarho <andyschima@gmail.com>
2022-11-30 14:57:40 +0100

    put favicon back in

--- src/routes/components/MainBody.jsx
+++ src/routes/components/MainBody.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 const MainBody = () =>{
     const mainContent = useSelector(state => state.mainContent);
-    const altImg = "public/1F3AE.svg";
+    const altImg = "/1F3AE.svg";
     console.log(mainContent);
     return (
         <div className="flex flex-col w-full items-center justify-evenly pt-5 gap-y-5">
